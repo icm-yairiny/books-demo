1aeca73bb6a4d7d64bf4e33950e9278e93be5726
icm-yairiny <yiny@icm-consulting.com.au>
2013-03-29 16:03:05 +1100

    updated sql for part2

--- sql/script1.sql
+++ sql/script1.sql
@@ -9,6 +9,5 @@ create table book (
        id serial primary key,
        author_id integer not null references author(id),
        title text not null,
-       isbn text not null,
        description text not null
 );
