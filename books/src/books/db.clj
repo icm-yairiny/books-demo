7dec8141be4012a3db92414e76ffd24714b66d08
icm-yairiny <yiny@icm-consulting.com.au>
2013-03-30 13:45:13 +1100

    adding part3/4 support

--- books/src/books/db.clj
+++ books/src/books/db.clj
@@ -11,3 +11,23 @@
 
 (defentity book
   (belongs-to author {:fk :author_id}))
+
+(defn get-author
+  "gets an author by id"
+  [id]
+  (first (select author (where {:id id}))))
+
+(defn find-authors
+  "finds authors by passing the supplied filter as the where parameters, filter should be a map"
+  [filter]
+  (select author (where filter)))
+
+(defn get-book
+  "gets a book by id"
+  [id]
+  (first (select book (where {:id id}))))
+
+(defn find-books
+  "finds books by passing the supplied filter as the where parameters, filter should be a map"
+  [filter]
+  (select book (where filter)))
--- books/src/books/handler.clj
+++ books/src/books/handler.clj
@@ -2,11 +2,55 @@
   (:use compojure.core)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
-            [ring.util.response :as resp]))
+            [ring.util.response :as resp]
+            [ring.middleware.json :as json]
+            [books.db :as db]))
+
+(defn- to-int
+  "converts a string to an integer"
+  [s]
+  (Integer/valueOf s))
+
+(def ^:private conversions
+  "a map of parameter name and converter"
+  {:author_id to-int
+   :book_id to-int})
+
+(defn- convert-params
+  "converts specific parameters to their proper types"
+  [params]
+  (reduce (fn [m [k v]]
+            (if-let [converter-fn (conversions k)]
+              (assoc m k (converter-fn v))
+              m))
+          params params))
+
+(defn- convert-params-handler
+  "a compojure handler to convert incoming parameters"
+  [app]
+  (fn [{params :params :as req}]
+    (app (assoc req :params (convert-params params)))))
+
+(defroutes data-routes
+  (GET "/authors/:id" [id]
+       (resp/response
+        {:author (db/get-author (to-int id))}))
+  (GET "/authors" {params :params}
+       (resp/response
+        {:authors (db/find-authors params)}))
+  (GET "/books/:id" [id]
+       (resp/response
+        {:book (db/get-book (to-int id))}))
+  (GET "/books" {params :params}
+       (resp/response
+        {:books (db/find-books params)})))
 
 (defroutes app-routes
   (GET "/" [] (resp/redirect "/index.html"))
   (route/resources "/")
+
+  (context "/data" []
+           (-> data-routes convert-params-handler json/wrap-json-response))
 
   (route/not-found "Not Found"))
 
